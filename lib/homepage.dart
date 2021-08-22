960e456dfb7b40e410bc67263a9690f5f0651178
Paul Imoke <pauleke65@gmail.com>
2021-08-22 19:30:19 +0000

    Version 2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hng_task2/details.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:rive/rive.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -11,28 +11,111 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController nameController = TextEditingController();
-  TextEditingController colorController = TextEditingController();
-  TextEditingController bookController = TextEditingController();
-  TextEditingController foodController = TextEditingController();
+  RiveAnimationController _controller;
+  bool nameSet = false;
+  bool noName = false;
+
+  /// Toggles between play and pause animation states
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = OneShotAnimation('Hover/Select', autoplay: false);
+  }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.white,
       body: Container(
-        padding: EdgeInsets.only(
-          top: height * 0.05,
-        ),
+        padding: EdgeInsets.symmetric(
+            vertical: height * 0.05, horizontal: width * 0.05),
         child: Column(
           children: [
             Text(
               "HNG Task 2",
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-            Text("brought to you by"),
+            SizedBox(height: 30),
+            Container(
+              height: noName ? 200 : 0,
+              width: 200,
+              child: RiveAnimation.asset(
+                "assets/gift.riv",
+              ),
+            ),
+            Container(
+              height: noName ? 0 : 200,
+              width: 200,
+              child: RiveAnimation.asset(
+                "assets/my_avatar.riv",
+                stateMachines: ["state1"],
+                controllers: [_controller],
+              ),
+            ),
             SizedBox(height: 20),
+            nameSet
+                ? Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                            noName
+                                ? "Your name is a gift. But,\nIt's one you can"
+                                : "Nice to Meet You,",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(noName ? "Share" : nameController.text,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ],
+                  )
+                : Container(),
+            SizedBox(height: 20),
+            Container(
+                width: width * 0.9,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                      labelText: "Your Name",
+                      border:
+                          OutlineInputBorder(borderSide: BorderSide(width: 1))),
+                )),
+            MaterialButton(
+              minWidth: width * 0.9,
+              onPressed: () {
+                setState(() {
+                  if (nameController.text.trim() != "") {
+                    noName = false;
+                    nameSet = true;
+                    _controller.isActive = true;
+                  } else {
+                    noName = true;
+                    nameSet = true;
+                  }
+                });
+              },
+              child: Text("Submit"),
+              color: Colors.yellow,
+              height: 50,
+              elevation: 0,
+            ),
+            SizedBox(height: 20),
+            Text("brought to you by"),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -42,41 +125,6 @@ class _HomePageState extends State<HomePage> {
                 imageLinks(
                     image: 'assets/i4g.png', link: "https://ingressive.org/"),
               ],
-            ),
-            SizedBox(height: 20),
-            textFieldStyle(
-                labelText: "Your Name",
-                width: width,
-                controller: nameController),
-            textFieldStyle(
-                labelText: "Favorite Color",
-                width: width,
-                controller: colorController),
-            textFieldStyle(
-                labelText: "Favorite Book",
-                width: width,
-                controller: bookController),
-            textFieldStyle(
-                labelText: "Favorite Food",
-                width: width,
-                controller: foodController),
-            SizedBox(height: 20),
-            MaterialButton(
-              minWidth: width * 0.9,
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => Details(
-                            name: nameController.text.trim(),
-                            color: colorController.text.trim(),
-                            book: bookController.text.trim(),
-                            food: foodController.text.trim())));
-              },
-              child: Text("Submit"),
-              color: Colors.white,
-              highlightColor: Colors.blue,
-              height: 50,
             ),
           ],
         ),
@@ -94,25 +142,12 @@ MaterialButton imageLinks({String image, String link}) {
     },
     child: Container(
       padding: EdgeInsets.all(10),
-      height: 100,
-      width: 100,
+      height: 70,
+      width: 70,
       decoration: BoxDecoration(
         color: Colors.white,
       ),
       child: Image.asset(image),
     ),
   );
-}
-
-Container textFieldStyle(
-    {String labelText, double width, TextEditingController controller}) {
-  return Container(
-      width: width * 0.9,
-      margin: EdgeInsets.symmetric(vertical: 10),
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-            labelText: labelText,
-            border: OutlineInputBorder(borderSide: BorderSide(width: 1))),
-      ));
 }
